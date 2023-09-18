0b4445ee558a1120270b03385fc33223a762a8f1
FricardoG <74424576+FricardoG@users.noreply.github.com>
2023-09-18 08:24:31 -0600

    Added Order-Product (items) association

--- db/models/order.model.js
+++ db/models/order.model.js
@@ -1,5 +1,6 @@
 const { Model, DataTypes, Sequelize } = require('sequelize');
-const { CUSTOMER_TABLE } = require('./customer.model')
+const { CUSTOMER_TABLE } = require('./customer.model');
+const { ORDER_PRODUCT_TABLE } = require('./order-product.model');
 
 const ORDER_TABLE = 'orders';
 
@@ -31,6 +32,12 @@ class Order extends Model{
   static associate(models){
     this.belongsTo(models.Customer,{
       as: 'customer',
+    });
+    this.belongsToMany(models.Product,{
+      as: 'items',
+      through: models.OrderProduct,
+      foreignKey: 'orderId',
+      otherKey: 'productId',
     });
   }
 
