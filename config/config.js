c38062e7528d35893e92c763f7b6efe2437d9457
FricardoG <74424576+FricardoG@users.noreply.github.com>
2023-09-23 12:18:49 -0600

    Applied dbURL, development / production environment connection

--- config/config.js
+++ config/config.js
@@ -4,12 +4,14 @@ require('dotenv').config(); // Load configuration from environment variables usi
 const config = {
   // Set the environment to the value of NODE_ENV environment variable, default to 'dev'.
   env: process.env.NODE_ENV || 'dev',
+  isProd: process.env.NODE_ENV == 'production',
   port: process.env.PORT || 3000,
   dbUser : process.env.DB_USER,
   dbPassword : process.env.DB_PASSWORD,
   dbHost: process.env.DB_HOST,
   dbName: process.env.DB_NAME,
   dbPort: process.env.DB_PORT,
+  dbURL: process.env.DATABASE_URL,
 }
 
 module.exports = { config }
